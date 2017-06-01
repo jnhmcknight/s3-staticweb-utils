4eec66cf45f87bb6c0be6a4526bb7e09ce33f297
Jared McKnight <jmcknight@archertech.ca>
2017-06-01 08:58:23 -0400

    adds HEAD to allowedmethods

--- app/s3.py
+++ app/s3.py
@@ -43,7 +43,8 @@ BUCKET_CORS = {
             '*'
         ],
         'AllowedMethods': [
-            'GET'
+            'GET',
+            'HEAD'
         ],
         'AllowedHeaders': [
             'Authorization',
